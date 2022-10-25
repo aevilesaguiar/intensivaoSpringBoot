b5274fe5f8a8d03609dda17e5fad2d0413911698
aevilesaguiar <aeviles@hotmail.com>
2022-10-25 09:19:47 -0300

    Initial commit

---
+++ src/test/java/com/intensivaosp/app/IntensivaoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intensivaosp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivaoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
